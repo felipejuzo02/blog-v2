ddfa11ee48ea2eb4e6f5a5fac8cb9f54d99d3143
felipejuzo02 <felipejuzo@hotmail.com>
2023-04-04 21:52:13 -0300

    feat: creation app button

---
+++ client/src/components/AppButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-btn
+    v-bind="$attrs"
+    class="app-button fit"
+    padding="13px"
+    rounded
+    size="14px"
+    unelevated
+  >
+    <template #loading>
+      <div class="spinner">
+        <q-icon
+          name="rotate_right"
+          size="28px"
+        />
+      </div>
+    </template>
+  </q-btn>
+</template>
+
+<script>
+export default {
+  name: 'AppButton'
+}
+</script>
+
+<style lang="scss">
+.app-button {
+  max-width: 200px;
+  background-color: $primary;
+  color: white;
+
+  &:hover {
+    background-color: $hover;
+  }
+
+  &:disabled {
+    background-color: red;
+    color: white;
+  }
+}
+
+.spinner {
+  animation: spinnerAnimation 1s infinite normal;
+}
+
+@keyframes spinnerAnimation {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
